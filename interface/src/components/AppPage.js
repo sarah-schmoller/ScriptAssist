67a672919f3db078cda5af44c659da6cb4c07135
sarah-schmoller <schmo065@umn.edu>
2020-07-07 13:06:42 -0500

    Cleanup

--- interface/src/components/AppPage.js
+++ interface/src/components/AppPage.js
@@ -1,31 +1,15 @@
 
 import React, {
 } from 'react';
-
-//import { Link } from "react-router-dom";
 import '../styles/appPageStyles.css';
 import axios from "axios";
-//import { useNavigate } from 'react-router-dom';
-//import '../nlg/chatbot'
-
-const theme = {
-   blue: {
-     default: "#3f51b5",
-     hover: "#283593"
-   },
-   pink: {
-     default: "#e91e63",
-     hover: "#ad1457"
-   }
- };
-
 
 function HomePage() {
 
   function generateResponse() {
-    console.log("IN HERE");
+
     async function test() {
-      console.log("IN HERE");
+
       let headers = {
         'headers': {
           'Content-Type': 'application/json',
@@ -35,35 +19,22 @@ function HomePage() {
           'Access-Control-Allow-Credentials': 'true'
         }
       }
+
       let dropdown = document.getElementById("dropdown15");
-      console.log("SELECTED");
-      console.log(dropdown.value);
       let reqInstance = axios.create(headers);
 
       let table = document.getElementById("table");
       let rows = table.getElementsByTagName('tr');
       let context;
-      console.log("LENGTH");
-      console.log(rows.length);
+
       if (rows.length <= 1) {
         context = ''
       } else {
-        console.log("HERE");
-        console.log(rows[rows.length-2].getElementsByTagName('td')[0].getElementsByTagName('textarea')[0])
-        console.log(rows[rows.length-2].getElementsByTagName('td')[0].getElementsByTagName('textarea')[0].value)
         context = rows[rows.length-2].getElementsByTagName('td')[0].getElementsByTagName('textarea')[0].value;
-        console.log(context);
       }
       let tester =  await reqInstance.post(`http://127.0.0.1:5000/`, {style: dropdown.value.toLowerCase(), context: context});
-      console.log("IN HERE");
-      console.log("RESULT");
-      console.log(tester);
-
-      console.log(rows[rows.length-1]);
       rows[rows.length-1].getElementsByTagName('td')[0].getElementsByTagName('textarea')[0].innerHTML = tester.data.response;
       rows[rows.length-1].getElementsByTagName('td')[0].getElementsByTagName('textarea')[0].value = tester.data.response;
-      console.log("ROWS");
-      console.log(rows[rows.length-1]);
       updateHeight();
     }
     test();
@@ -71,11 +42,9 @@ function HomePage() {
   }
 
   function createNewTurn() {
-    let table = document.getElementById("table");
+    let table = document.getElementById("dialogTurnsTableBody");
     let rows = table.getElementsByTagName('tr');
     let rowCount = rows.length;
-    console.log(rows[rowCount-1].getElementsByTagName('td')[0].getElementsByTagName('input')[0].value);
-    console.log(rows[rowCount-1]);
     let characterName;
     if (rowCount <= 1) {
       characterName = 'Character 2:';
@@ -113,8 +82,7 @@ function HomePage() {
     padding-left: 5px;
     margin-bottom: 10px;
     color: #0E012A;`);
-    console.log("STYLE");
-    console.log(textarea.style);
+
     column.appendChild(input);
     column.appendChild(textarea);
     row.appendChild(column)
@@ -122,23 +90,17 @@ function HomePage() {
   }
 
   function updateHeight(test) {
-    let table = document.getElementById("dialogTurnsTable");
+    let table = document.getElementById("dialogTurnsTableBody");
     let rows = table.getElementsByTagName('tr');
     for (let i = 0; i < rows.length; i++) {
       let element = rows[i].getElementsByTagName('td')[0].getElementsByTagName('textarea')[0];
-      console.log("STYLES");
-      console.log(element.style);
-      console.log(element.style.scrollHeight);
-      console.log(element.style.height);
-      console.log(element.scrollHeight);
       element.style.setProperty('height', element.scrollHeight + 'px');
       element.style.setProperty('max-height', element.scrollHeight + 'px');
-    //content[i].style.height = content[i].scrollHeight + 'px';
     }
   }
 
   return (
-    <body id='pageBody'>
+    <div id='pageBody'>
 
       <div id='widgetContainer'>
 
@@ -150,7 +112,7 @@ function HomePage() {
           <div id='divider'>
           </div>
           <div class='actions' id='styleSelector'>
-            <text id='styleSelectorText'>Choose a dialog style.</text>
+            <span id='styleSelectorText'>Choose a dialog style.</span>
             <div id='styleSelectorDropdownDiv'>
               <select id='styleSelectorDropdown'>
                 <option selected>Shakespeare</option>
@@ -162,19 +124,21 @@ function HomePage() {
             </div>
           </div>
           <div class='actions' id='download'>
-            <text id='scriptDownloadText'>Download your script.</text>
+            <span id='scriptDownloadText'>Download your script.</span>
             <form><button id='scriptDownloadButton'>Download TXT</button></form>
           </div>
         </div>
 
         <div id='widgetBody'>
             <table id='dialogTurnsTable'>
-              <tr class='dialogTurnRow'> 
-                <td class='dialogTurnColumn'>
-                  <input class='dialogTurnInput' defaultValue="Character 1:"></input>
-                  <textarea class='dialogTurnTextArea' placeholder="Click here to write your first turn of dialog, or choose 'Auto' to autogenerate a turn." onInput={(e) => updateHeight(e)}></textarea>
-                </td>
-              </tr>
+              <tbody id= 'dialogTurnsTableBody'>
+                <tr class='dialogTurnRow'> 
+                  <td class='dialogTurnColumn'>
+                    <input class='dialogTurnInput' defaultValue="Character 1:"></input>
+                    <textarea class='dialogTurnTextArea' placeholder="Click here to write your first turn of dialog, or choose 'Auto' to autogenerate a turn." onInput={(e) => updateHeight(e)}></textarea>
+                  </td>
+                </tr>
+              </tbody>
             </table>
             <div id='widgetButtonsDiv'>
               <button class='button' id='addTurnButton'><span onClick={() => createNewTurn()}>+ Add Turn</span></button>
@@ -183,7 +147,7 @@ function HomePage() {
           </div>
       </div>
 
-    </body>
+    </div>
   ); 
 }
 
